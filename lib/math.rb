b6f1da6f1465a7170d1e334eace6e39c63f06162
Sophia Song <ososanne@gmail.com>
2019-05-06 18:03:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,9 +19,9 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  
+  num ** 2
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+   num1 + (( num2 * num3 ) / num4 )
 end
